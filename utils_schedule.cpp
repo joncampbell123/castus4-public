d2076bee68fede71a23ee128ff5ff3f32d3dcbe8
Alex Elsayed <eternaleye@gmail.com>
2019-03-28 13:32:32 -0700

    Refactor loop() to be somewhat more idiomatic

--- utils_schedule.cpp
+++ utils_schedule.cpp
@@ -63,14 +63,20 @@ bool write(Castus4publicSchedule &schedule) {
 void loop(Castus4publicSchedule &schedule,
         std::function<void (Castus4publicSchedule::ScheduleItem &current_item,
                     Castus4publicSchedule::ScheduleItem &next_item)> logic) {
-    for (auto schedule_item=schedule.schedule_items.begin();schedule_item!=schedule.schedule_items.end();) {
-        auto current_item = schedule_item;
-        schedule_item++;
-        if (schedule_item == schedule.schedule_items.end()) break;
+    // Skip an item at the start (it will be manually visited)
+    for (auto schedule_item = ++schedule.schedule_items.begin();
+              schedule_item !=  schedule.schedule_items.end();
+              ++schedule_item) {
+        // Stash the current iterator
+        auto tmp = schedule_item;
+        // Bind the "next item", walking the stashed iterator back a step
+        auto& next = *(tmp--);
+        // Bind the current item
+        auto& current = *tmp;
 
-        auto next_item = schedule_item;
-
-        if (is_valid(*current_item) && is_valid(*next_item)) logic(*current_item, *next_item);
+        if (is_valid(current) && is_valid(next)) {
+            logic(current, next);
+        }
     }
 }
 
